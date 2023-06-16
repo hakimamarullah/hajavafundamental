2a1457617f3dbe1cb9a0843949e66d6c2f797e87
hakimamarullah <hakimamarullah@gmail.com>
2023-06-16 21:47:45 +0700

    Initial commit

---
+++ src/main/java/com/juaracoding/hajavafundamental/TestTemplate.java
@@ -0,0 +1,17 @@
+package com.juaracoding.hajavafundamental;
+/*
+IntelliJ IDEA 2022.2.2 (Community Edition)
+Build #IC-222.4167.29, built on September 13, 2022
+Runtime version: 17.0.4+7-b469.53 amd64
+@Author hakim a.k.a. Hakim Amarullah
+Java Developer
+Created on 6/16/2023 8:38 PM
+@Last Modified 6/16/2023 8:38 PM
+Version 1.0
+*/
+
+public class TestTemplate {
+    public static void main(String[] args) {
+        System.out.println("Hell Yeah");
+    }
+}
