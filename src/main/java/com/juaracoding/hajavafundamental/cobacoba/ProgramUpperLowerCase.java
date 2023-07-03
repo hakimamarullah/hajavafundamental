80a81c7fbe9e741c173cf18af5b34902e08cea22
hakimamarullah <hakimamarullah@gmail.com>
2023-07-03 22:02:25 +0700

    Create main method

--- src/main/java/com/juaracoding/hajavafundamental/cobacoba/ProgramUpperLowerCase.java
+++ src/main/java/com/juaracoding/hajavafundamental/cobacoba/ProgramUpperLowerCase.java
@@ -10,7 +10,24 @@ Created on 7/3/2023 9:03 PM
 Version 1.0
 */
 
+import java.util.Scanner;
+
 public class ProgramUpperLowerCase {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+       if(isHuruf(input.charAt(0))){
+           if(isLowerCase(input.charAt(0))){
+               System.out.printf("%s bukan huruf kapital", input.charAt(0));
+           }else {
+               System.out.printf("%s adalah huruf kapital", input.charAt(0));
+           }
+       }else{
+           System.out.printf("%s bukan huruf", input.charAt(0));
+       }
+    }
 
     /**
      * Returns boolean true if the character is digit 0..9
